535c1f7374be7063d1db0fe462ecbe4ce6181261
rdsr <rdsr@yahoo-inc.com>
2013-12-19 17:45:12 +0530

    test, in progress

---
+++ test/clj/dist_copy/splits.clj
@@ -0,0 +1,23 @@
+(ns dist-copy.splits
+  (:use [clojure.test]
+        [dist-copy.splits])
+  (:import [java.io File]
+           [org.apache.hadoop.fs Path FileSystem]
+           [org.apache.hadoop.conf Configuration]
+           [org.apache.hadoop.hdfs MiniDFSCluster]))
+
+
+(defn- create-cluster [conf]
+  (-> conf
+      org.apache.hadoop.hdfs.MiniDFSCluster$Builder.
+      .build))
+
+(defn- destroy-cluster [cluster]
+  (when (.isClusterUp cluster)
+    (.shutdown cluster)))
+
+
+
+(deftest 
+  (testing "splits file creation"
+    (is (= 0 1))))
