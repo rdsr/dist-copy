3ce7eaec22a1f6144a89a10852a1463a21f4d0e3
rdsr <rdsr@yahoo-inc.com>
2013-12-30 23:28:18 +0530

    backup-checkin

--- src/clj/dist_copy/client.clj
+++ src/clj/dist_copy/client.clj
@@ -1,16 +1,20 @@
 (ns dist-copy.client
   (:require [clojure.pprint :as pp])
-  (:use [dist-copy.constants :as c])
-  (:import [org.apache.hadoop.yarn.conf YarnConfiguration]
+  (:use [clojure.string :only (join)] 
+        [dist-copy.util] 
+        [dist-copy.constants :as c])
+  (:import [java.io File] 
+           [org.apache.hadoop.yarn.conf YarnConfiguration]
            [org.apache.hadoop.yarn.client.api YarnClient]
-           [org.apache.hadoop.yarn.api.records QueueInfo 
+           [org.apache.hadoop.yarn.api ApplicationConstants ApplicationConstants$Environment] 
+           [org.apache.hadoop.yarn.api.records QueueInfo
             NodeState NodeReport QueueUserACLInfo QueueACL]))
 
 
-(defn- queue-info 
+(defn- queue-info
   [^YarnClient yc]
   (let [queue (-> yc .getConfig (.get c/queue "default"))
-        ^QueueInfo qi (.getQueueInfo yc queue)] 
+        ^QueueInfo qi (.getQueueInfo yc queue)]
     {:name (.getQueueName qi)
      :current-capacity (.getCurrentCapacity qi)
      :max-capacity (.getMaximumCapacity qi)
@@ -29,24 +33,32 @@
 
 
 (defn- acls-info
-  [yc]
-  (for [^QueueUserACLInfo acl-info (.getQueueAclsInfo yc) 
+  [^YarnClient yc]
+  (for [^QueueUserACLInfo acl-info (.getQueueAclsInfo yc)
         ^QueueACL user-acl (.getUserAcls acl-info)]
     [:queue (.getQueueName acl-info)
      :user  (.name user-acl)]))
-                                          
 
-(defn cluster-reports 
+
+(defn cluster-reports
   [^YarnClient yc]
   {:node-managers (-> yc .getYarnClusterMetrics .getNumNodeManagers)
    :nodes (nodes-info yc)
    :queue-info (queue-info yc)
    :acls-info (acls-info yc)})
-   
+
 
 (defn print-cluster-reports
-  [yc]
-  (pp/pprint (cluster-reports yc)))
+  [yc] (pp/pprint (cluster-reports yc)))
+
+
+(defn- mk-conf
+  [kvs] (let [conf (YarnConfiguration.)]
+          (doseq [[k v] kvs]
+            (.set conf (name k) (name v)))
+          conf))
+  
+
 
 (defn- yarn-client
   ^YarnClient [^YarnConfiguration conf]
@@ -54,17 +66,78 @@
     (.init conf)
     (.start)))
 
-(defn- run [yc])
 
-(defn copy 
+(defn- app-classpath
+  [conf]
+  ;; todo Add log4j
+  (join
+    File/pathSeparatorChar
+    (map #(.trim %)  
+         (concat ["$CLASSPATH" "."]  
+                 (.getStrings conf  
+                   YarnConfiguration/YARN_APPLICATION_CLASSPATH 
+                   YarnConfiguration/DEFAULT_YARN_APPLICATION_CLASSPATH)))))
+
+(defn- mk-am-env
+  [conf]
+  ;; Currently just set the classpath
+  {"CLASSPATH" (app-classpath conf)})   
+
+(defn- mk-am-cmd
+  [] (join 
+       " "
+       ["$JAVA_HOME/bin/java"
+       "-Xmx" 
+       (str (.getInt conf "dist.copy.am.memory.mb" 512) "m") 
+       (str "1>" ApplicationConstants/LOG_DIR_EXPANSION_VAR "dist-copy.stdout")
+       (str "2>" ApplicationConstants/LOG_DIR_EXPANSION_VAR "dist-copy.stderr")]))
+        
+
+(defn- mk-AM-container
+  "Setup container launch context for Application Master"
+  []
+  (let [amc (mk-record ContainerLaunchContext)
+        local-resources {:jar (am-jar conf)}]
+    (doto amc
+      (.setEnv (mk-am-env conf))
+      (.setCommands [(mk-am-cmd)])
+      (.setTokens (security-tokens))))
+    
+    
+                         
+    
+  ;; local-resource
+  ;; log file
+  ;; classpath
+  ;; command
+  )
+
+(defn- mk-AM
+  "Setup Application Master"
+  [^YarnClientApplication app ^YarnConfiguration conf]
+  (let [appContext (.getApplicationSubmissionContext app)]
+    (.setApplicationName appContext (conf))
+  ;; Create application submission context
+  ;; Create launch container
+  ;; security tokens
+  )
+
+(defn- run [yc]
+  (let [conf (.getConfig yc)
+        app (.createApplication yc)
+        app-response (.getNewApplicationRespone app)]
+    ;; Todo cap am-memory to max container memory
+    (mk-AM app conf)
+
+(defn copy
   [& kvargs]
-  (let [conf (YarnConfiguration.)
-        kvs (apply hash-map kvargs)
+  (let [conf (mk-conf kvargs)
         yc (yarn-client conf)]
     (print-cluster-reports yc)
-    (run yc)))
-    
+    (run yc)
+    (.stop yc)))
+
 
 ;(:app-name "" :priority "" :queue "" :am-memory "")
-           
+
 ;(copy)
---
+++ src/clj/dist_copy/util.clj
@@ -0,0 +1,40 @@
+(ns dist-copy.util
+  (:import [java.io File FileOutputStream BufferedOutputStream]
+           [java.util.zip ZipOutputStream ZipEntry] 
+           [org.apache.hadoop.yarn.util Records]))
+
+
+(defn mk-record [clazz]
+  (Records/newRecord class))
+
+
+(defn- jar-file [resource]
+  (-> resource .getPath (.split "!") first))
+
+
+(defn- create-zip [base-dir]
+  (let [zip (str (System/getProperty "java.io.tmpdir") "/dist-copy.zip")]
+    (with-open [os (-> zip FileOutputStream. BufferedOutputStream. ZipOutputStream.)]
+      (doseq [f (-> base-dir File. file-seq)]
+        (when (.isFile f)
+          (.putNextEntry os 
+            (ZipEntry. (-> f str (.replace  base-dir "")))))))))
+    
+
+(defn- zip-file [resource rel-dir]
+  (let [rel-dir (if (.startsWith rel-dir "/") 
+                  (subs rel-dir 1)
+                  rel-dir)
+        dir (-> resource .getPath)]
+    (create-zip 
+      (.substring dir 0 (.indexOf dir rel-dir)))))
+              
+
+(defn archive [clazz]
+  (let [name (str "/" 
+                  (.. clazz getName (replace "." "/")) 
+                  ".class")
+        resource (.getResource clazz name)]
+    (if (= "jar" (.getProtocol resource))
+      (jar-file resource)
+      (zip-file resource name))))
